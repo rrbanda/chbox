1dd2bacca08fe91828cb86cd675c1ddf381c3973
Raghuram Banda <rbanda@redhat.com>
2024-03-28 09:26:45 -0400

    Delete brain_seg_ui directory

--- brain_seg_ui/README.md
+++
@@ -1,3 +0,0 @@
-# brain_seg_ui
-
-A Flask ui for testing the 3D Attention-based UNet Model for segmenting Multimodal Brain Tumors
--- brain_seg_ui/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""Application entry point."""
-from segmentation import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
